ca692227d004e731df1d642d3c41ed8c9c6314a5
johnbuenodev <johnbuenodev@gmail.com>
2021-12-19 23:44:26 -0300

    observable rxjs referencia.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'observableApp';
+  obsObj : any;
+
+  ngOnInit(): void {
+    this.obsObj = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(4);
+      subscriber.next(5);
+      setTimeout(() => {
+        subscriber.next(3);
+        subscriber.complete();
+      },2000) 
+    });
+  }
+
+  callObservable() {
+    console.log('just before subscribe');
+    this.obsObj.subscribe({
+      next(x : any) { 
+        console.log('got value ' + x);
+      },
+      error(err : any) {
+        console.log('wrong occurred: ' + err)
+      },
+      complete() {
+        console.log('Complete!');
+      }
+    });
+    console.log('Just after subscribe');
+  }
+  
+
+  
+
 }
